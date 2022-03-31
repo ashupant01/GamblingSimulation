4380122974686e6cc60d31beccb92b9459d6a4e8
Ashu Pant <99981051+ashupant01@users.noreply.github.com>
2022-03-31 08:45:58 +0530

    Add files via upload
    UC1 is Added

---
+++ GamblingSimulation.java
@@ -0,0 +1,17 @@
+package GamblingSimulation;
+
+import java.util.Scanner;
+
+public class GamblingSimulation {
+
+	int Stack = 100;
+	int EveryDayBet = 1;
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome To Gambling Simulation");
+		
+
+	}
+
+}
