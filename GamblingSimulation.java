eee9cf187f80fd96f67213ebc2a5f44fb2e36577
Ashu Pant <99981051+ashupant01@users.noreply.github.com>
2022-03-31 09:02:12 +0530

    Add files via upload
    Added Wins Or Lose To The Gambling Simulation Program

---
+++ GamblingSimulation.java
@@ -0,0 +1,26 @@
+package GamblingSimulation;
+
+import java.util.Scanner;
+
+public class GamblingSimulation {
+
+	int Stack = 100;
+	int EveryDayBet = 1;
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome To Gambling Simulation");
+		
+		if (Math.random() <0.5) {
+		
+        System.out.println("Wins");
+		}
+		else
+		{
+	    System.out.println("Lose");
+		}
+		}
+			
+	}
+
+
